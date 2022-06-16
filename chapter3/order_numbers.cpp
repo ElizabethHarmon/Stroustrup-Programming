65de6ea1e0ecea8c66524e1843f232bf42824b2b
Elizabeth Harmon <betholiviaharmon@gmail.com>
2022-06-16 12:52:30 +0100

    Sort 3 numbers

---
+++ chapter3/order_numbers.cpp
@@ -0,0 +1,20 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    int i1;
+    int i2;
+    int i3;
+    
+    cout << "Enter three integers: ";
+    cin >> i1 >> i2 >> i3;
+    vector<int> v{i1, i2, i3};
+    sort(v.begin(), v.end());
+
+    cout << "Sorted:\n";
+    for (auto x : v)
+        cout << x << " ";
+    cout << '\n';    
+
+    return 0;
+}
